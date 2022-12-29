6977b5dca57f5c65d2e37cb6c60d54e1c1b642a0
David Whitney <codifyamplify@gmail.com>
2022-12-28 20:33:37 -0500

    commit tree dba5959403165356612710b1f7fb00230226835c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,54 @@
 class Triangle
   # write code here
+  # write macros
+  attr_reader :a, :b, :c
+  # initialization
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  # instance method #kind what returns its type as a symbol
+  # this instance method should raise a custom error TriangleError if the triangle is invalid
+
+  # LETS WRITE A BETTER KIND
+  def kind
+    # greater_than_zero(a, b, c)
+    # triangle_inequality
+    check_triangle
+
+
+          if a == b && b == c
+            :equilateral
+          elsif a == b || b == c || c == a
+            :isosceles
+          else
+            :scalene
+          end
+  end
+
+  # write a method to check if sides are greater than zero
+  def sides_greater_than_zero?
+    # i want a true answer here to validate sides are truly greater than zero
+    # a > 0 && b > 0 && c > 0
+    [a, b, c].all?(&:positive?)
+  end
+  # write a method that makes sure the sum of any two sides is greater than the third side
+  def triangle_inequality?
+    a + b > c && b + c > a && c + a > b
+  end
+
+# write a check_triangle method to make sure it passes the sides_greater_than_zero method
+# as well as the triangle_inequality method
+def check_triangle
+  raise TriangleError unless sides_greater_than_zero? && triangle_inequality?
 end
+
+  # write custom error class
+  class TriangleError < StandardError
+  end
+
+end
+
+# newbie = Triangle.new(1, 2, 3)
+# puts newbie.kind
